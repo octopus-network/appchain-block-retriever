22bd9202bbbfaae9c15c6f76a1983e6e32ee1941
Li Smith <jinrishouji@sina.com>
2021-07-13 17:03:59 +0800

    add cmd param ChaidId and to Height export_bak_file

--- down-bak-file.js
+++ down-bak-file.js
@@ -13,7 +13,7 @@ const arweave = Arweave.init({
 const ardb = new ArDB.default(arweave);
 
 
-async function export_bak_file() {
+async function export_bak_file(ChaidId, targetBlockHight) {
     const path = './block.bak'
 
     // This is the same as doing:
@@ -24,14 +24,17 @@ async function export_bak_file() {
         console.error(err)
     }
 
-    const limit = 3;
-    for (var i = 0; i < limit; i++) {
-        const txs = await ardb.search('transactions').tag('chain-id', '9999').tag('block-high', i.toString()).limit(1).find();
+    for (var i = 0; i <= targetBlockHight; i++) {
+        const txs = await ardb.search('transactions').tag('ChaidId', ChaidId.toString()).tag('Height', i.toString()).limit(1).find();
 
+        if (!txs[0]) {
+            console.log("####### get " + i + " block data is null, ", "ChaidId:", ChaidId.toString(), "targetBlockHight", i.toString());
+            continue;
+        }
         // Get the data decode as string data
         arweave.transactions.getData(txs[0]._id, { decode: true, string: true }).then(data => {
             //console.log(txs)
-            console.log("#########");
+            //console.log("#########");
             console.log(txs[0]._tags)
             console.log(txs[0]._data)
 
@@ -43,4 +46,8 @@ async function export_bak_file() {
     }
 }
 
-export_bak_file()
+//node down-bak-file.js 9999 3
+const args = process.argv.slice(2)
+
+console.log("ChaidId:", args[0], "targetBlockHight", args[1]);
+export_bak_file(args[0], args[1])
