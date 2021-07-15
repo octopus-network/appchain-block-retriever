eef49592d1ecd448541a19b440daaf37949afa32
Li Smith <jinrishouji@sina.com>
2021-07-16 01:08:36 +0800

    add kyve version download

--- down-bak-file.js
+++ down-bak-file.js
@@ -46,8 +46,41 @@ async function export_bak_file(ChaidId, targetBlockHight) {
     }
 }
 
+async function export_bak_file_from_kyve(ChaidId, targetBlockHight) {
+  const path = './block.bak'
+
+  // This is the same as doing:
+  try {
+      fs.unlinkSync(path)
+      //file removed
+  } catch (err) {
+      console.error(err)
+  }
+
+  for (var i = 0; i <= targetBlockHight; i++) {
+      const txs = await ardb.search('transactions').tag('ChaidId', ChaidId.toString()).tag('Height', i.toString()).limit(1).find();
+
+      if (!txs[0]) {
+          console.log("####### get " + i + " block data is null, ", "ChaidId:", ChaidId.toString(), "targetBlockHight", i.toString());
+          continue;
+      }
+      // Get the data decode as string data
+      arweave.transactions.getData(txs[0]._id, { decode: true, string: true }).then(data => {
+          //console.log(txs)
+          //console.log("#########");
+          console.log(txs[0]._tags)
+          console.log(txs[0]._data)
+
+          console.log(data);
+
+          fs.writeFile(path, JSON.parse(data), { flag: 'a' }, err => { console.log(err); })
+
+      });
+  }
+}
+
 //node down-bak-file.js 9999 3
 const args = process.argv.slice(2)
 
 console.log("ChaidId:", args[0], "targetBlockHight", args[1]);
-export_bak_file(args[0], args[1])
+export_bak_file_from_kyve(args[0], args[1])
