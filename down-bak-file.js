05c9ecb960278d573aa6b3c61c5761ca1e6bdfab
Li Smith <jinrishouji@sina.com>
2021-08-07 09:20:57 +0800

    update ChainId spell

--- down-bak-file.js
+++ down-bak-file.js
@@ -13,7 +13,7 @@ const arweave = Arweave.init({
 const ardb = new ArDB.default(arweave);
 
 
-async function export_bak_file(ChaidId, targetBlockHight) {
+async function export_bak_file(ChainId, targetBlockHight) {
     const path = './block.bak'
 
     // This is the same as doing:
@@ -25,10 +25,10 @@ async function export_bak_file(ChaidId, targetBlockHight) {
     }
 
     for (var i = 0; i <= targetBlockHight; i++) {
-        const txs = await ardb.search('transactions').tag('ChaidId', ChaidId.toString()).tag('Height', i.toString()).limit(1).find();
+        const txs = await ardb.search('transactions').tag('ChainId', ChainId.toString()).tag('Height', i.toString()).limit(1).find();
 
         if (!txs[0]) {
-            console.log("####### get " + i + " block data is null, ", "ChaidId:", ChaidId.toString(), "targetBlockHight", i.toString());
+            console.log("####### get " + i + " block data is null, ", "ChainId:", ChainId.toString(), "targetBlockHight", i.toString());
             continue;
         }
         // Get the data decode as string data
@@ -46,7 +46,28 @@ async function export_bak_file(ChaidId, targetBlockHight) {
     }
 }
 
-async function export_bak_file_from_kyve(ChaidId, targetBlockHight) {
+async function export_bak_file_from_kyve_union(path, ChainId, i) {
+  const txs = await ardb.search('transactions').tag('ChainId', ChainId.toString()).tag('Height', i.toString()).limit(1).find();
+
+  if (!txs[0]) {
+      console.log("####### get " + i + " block data is null, ", "ChainId:", ChainId.toString(), "targetBlockHight", i.toString());
+      return;
+  }
+  // Get the data decode as string data
+  arweave.transactions.getData(txs[0]._id, { decode: true, string: true }).then(data => {
+      //console.log(txs)
+      //console.log("#########");
+      // console.log(txs[0]._tags)
+      // console.log(txs[0]._data)
+
+      // console.log(data);
+
+      console.log(i)
+      fs.writeFileSync(path, JSON.parse(data) + '\n', { flag: 'a' }, err => { console.log(err); })
+
+  });
+}
+async function export_bak_file_from_kyve(ChainId, targetBlockHight) {
   const path = './block.bak'
 
   // This is the same as doing:
@@ -58,29 +79,12 @@ async function export_bak_file_from_kyve(ChaidId, targetBlockHight) {
   }
 
   for (var i = 0; i <= targetBlockHight; i++) {
-      const txs = await ardb.search('transactions').tag('ChaidId', ChaidId.toString()).tag('Height', i.toString()).limit(1).find();
-
-      if (!txs[0]) {
-          console.log("####### get " + i + " block data is null, ", "ChaidId:", ChaidId.toString(), "targetBlockHight", i.toString());
-          continue;
-      }
-      // Get the data decode as string data
-      arweave.transactions.getData(txs[0]._id, { decode: true, string: true }).then(data => {
-          //console.log(txs)
-          //console.log("#########");
-          console.log(txs[0]._tags)
-          console.log(txs[0]._data)
-
-          console.log(data);
-
-          fs.writeFile(path, JSON.parse(data), { flag: 'a' }, err => { console.log(err); })
-
-      });
+      await export_bak_file_from_kyve_union(path, ChainId, i);
   }
 }
 
 //node down-bak-file.js 9999 3
 const args = process.argv.slice(2)
 
-console.log("ChaidId:", args[0], "targetBlockHight", args[1]);
+console.log("ChainId:", args[0], "targetBlockHight", args[1]);
 export_bak_file_from_kyve(args[0], args[1])
